6df4ee175dea31a6fe0b2266738e2fe28205b077
Jim Haughwout <jim.haughwout@gmail.com>
2015-11-21 15:27:28 -0500

    First pass on Cartesian projection

--- gadmin_scan.py
+++ gadmin_scan.py
@@ -1,23 +1,35 @@
 import settings as s
 import utils
+import xform
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
+DEBUG = s.DEBUG
 
 poi_set = utils.import_measures(s.DATA_FILE)
 
-
-if s.DEBUG:
-    for poi in poi_set:
-        print "%s (%.4f, %.4f)" % (poi[s.NAME_KEY], 
-            poi[s.LAT_KEY], poi[s.LNG_KEY])
-
-    for poi_1 in poi_set:
-        print "\nFROM %s" % poi_1[s.NAME_KEY]
-        print "==================================="
-        for poi_2 in poi_set:
-            if poi_1 != poi_2:
-                utils.dist_calc(poi_1, poi_2, mode='gadm')
+# Create labels
+poi_names = list()
+poi_coords = list()
+for poi in poi_set:
+    poi_names.append(poi[s.NAME_KEY])
+    poi_coords.append((poi[s.LAT_KEY], poi[s.LNG_KEY]))
 
 
+labels_true = np.array(poi_names)
+raw_X = np.array(poi_coords)
+projected_X = np.array(xform.cart_projection(poi_set))
+X = StandardScaler().fit_transform(projected_X)
 
+if DEBUG:
+    c_lat, c_lng = xform.get_centroid(poi_set)
+    print "\nlabels_true is %s" % type(labels_true)
+    print labels_true
+    print "\nraw_X is %s" % type(raw_X)
+    print raw_X
+    print "\nCentroid is: (%.4f, %.4f)" % (c_lat, c_lng) 
+    print "\nprojected_X is %s" % type(projected_X)
+    print projected_X
+    print "\nX is %s" % type(X)
+    print X
 
